077cfe427e9b40728de5a01d6c8b0aeb674c6e6e
corbob <corbob@knoxy.ca>
2020-02-11 21:04:12 -0800

    Let's add some tables for reals this time ¯\_(  ʖ )_/

--- Build-StickerShowcase.ps1
+++ Build-StickerShowcase.ps1
@@ -15,5 +15,5 @@ foreach ($design in $finalDesigns) {
     $directory = $ExecutionContext.SessionState.Path.NormalizeRelativePath($design.PSParentPath, $PSScriptRoot)
     $image = $ExecutionContext.SessionState.Path.NormalizeRelativePath($design.PSPath, $PSScriptRoot)
     $file = $design.BaseName -replace "-final", ""
-    Add-Content -Value "${file}:<a href=`"$directory`"><img src=`"$image`" width=`"25%`"></a>`r`n" -Path $Path
+    Add-Content -Value "|${file}|<a href=`"$directory`"><img src=`"$image`" width=`"25%`"></a>|" -Path $Path
 }
